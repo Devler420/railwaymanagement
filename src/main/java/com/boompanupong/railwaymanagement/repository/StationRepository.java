0850a6a3dbad3298d317e264271441f87c2fa067
Boom P <boompanupong@gmail.com>
2024-12-09 16:53:18 +0700

    skeleton, h2 accessible

--- src/main/java/com/boompanupong/railwaymanagement/repository/StationRepository.java
+++ src/main/java/com/boompanupong/railwaymanagement/repository/StationRepository.java
@@ -4,8 +4,6 @@ import com.boompanupong.railwaymanagement.model.Station;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface StationRepository extends CrudRepository<Station, Long> {
 }
